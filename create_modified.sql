c16283a6f83fe6cbfce84d1e58cd367ed752f4b4
dliu234 <60945805+dliu234@users.noreply.github.com>
2020-02-20 20:45:13 -0600

    Add files via upload

---
+++ create_modified.sql
@@ -0,0 +1,39 @@
+drop table if exists Bid;
+drop table if exists Category;
+drop table if exists Item;
+drop table if exists User;
+create table Bid(
+    itemID int NOT NULL, 
+    amount float NOT NULL,
+    userID varchar(255) NOT NULL, 
+    time time NOT NULL, 
+    primary key(itemID, userID, time),
+    foreign key (itemID) references item(itemID),
+    foreign key (userID) references user(userID)
+);
+create table Category(
+    category varchar(255) NOT NULL,
+    primary key(category),
+);
+create table Item(
+    itemID int NOT NULL,
+    category varchar(255) NOT NULL,
+    ends time NOT NULL, 
+    first_bid float NOT NULL, 
+    name varchar(255) NOT NULL, 
+    started time NOT NULL, 
+    number_of_bids int NOT NULL, 
+    currently float NOT NULL, 
+    userID varchar(255) NOT NULL, 
+    description varchar(255),
+    primary key(itemID),
+    foreign key(userID) references user(userID)
+    foreign key(category) references category(category)
+);
+create table User(
+    rating int NOT NULL, 
+    userID varchar(255) NOT NULL,
+    location varchar(255),
+    country varchar(255),
+    primary key(userID)
+);
